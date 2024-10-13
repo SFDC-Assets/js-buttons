05d648e2d9053bcc312dff6239fb571838221a28
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-13 00:53:16 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p js -e work.shop
 sf project deploy start
-sf project deploy start -d destroy/ -w 20
+# sf project deploy start -d destroy/ -w 20
 sf demoutil user password set -p salesforce1 -g User -l User
 sf org open
